2ed0632551fa0e1f84488dc9620ba2fed918b529
lourdes-miazzo <108072073+lourdes-miazzo@users.noreply.github.com>
2024-05-06 18:52:12 -0300

    Update app.js

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ const app= express()
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(cors())
+app.get("/", (req, res)=>{
+  res.setHeader("Access-Controll-Allow-Credentials", "true")
+  res.send("Api is running...")
+})
 app.use(morgan("dev"))
 
 export default app
